c6aa4bccf67bc992518515aab5985ee8cb842b88
Lucas, Uki <Uki.Lucas@harman.com>
2019-07-16 17:29:54 -0400

    RANSAC quiz done!

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -61,22 +61,82 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
-std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+std::unordered_set<int> 
+Ransac(
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, 
+	int maxIterations, 
+	float distanceTol)
 {
+	// Declare an unordered set of the best results:
 	std::unordered_set<int> inliersResult;
+
+	// Seed the random function with time:
 	srand(time(NULL));
 	
-	// TODO: Fill in this function
+	/** 
+	 * Looping down from maxIterations
+	*/
+	while(maxIterations --)
+	{
+		// Declare an unordered set of random 2 points.
+		std::unordered_set<int> inliers;
 
-	// For max iterations 
+		// Randomly select 2 points from the cloud set
+		while(inliers.size() < 2)
+		{
+			// mod of cloud's size: results between 0 and the cloud size:
+			// we are using a set so we will not be able to pick the same point.
+			inliers.insert(rand()%(cloud->points.size()));
+		}
+		// Declare values for points (x1,y1), (x2, y2)
+		float x1, y1, x2, y2;
+		auto itr = inliers.begin(); // start from the begining of the set (of 2)
 
-	// Randomly sample subset and fit line
+		// grab the values from the cloud:
+		x1 = cloud->points[*itr].x;
+		y1 = cloud->points[*itr].y;
+		itr++;
+		x2 = cloud->points[*itr].x;
+		y2 = cloud->points[*itr].y;
 
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
+		// Use linear equasions to calculate the coefficients:
+		float a = (y1-y2);
+		float b = (x2-x1);
+		float c = (x1*y2-x2*y1);
 
-	// Return indicies of inliers from fitted line with most inliers
+		// Iterate thru all the cloud points:
+		for(int index = 0; index < cloud->points.size(); index++)
+		{
+			// if the inliers alrady contain the point index, then continue:
+			if(inliers.count(index) > 0)
+			{
+				continue;
+			}
+			// Get the point for a given index, to extract Z coordinates:
+			pcl::PointXYZ point = cloud->points[index];
+			float x3 = point.x;
+			float y3 = point.y;
+
+			// Calculate distance of the point:
+			// fabs = float absolute value
+			float d = fabs(a*x3+b*y3+c)/sqrt(a*a+b*b);
+
+			// if distance is less, or equal to the threshhold:
+			if( d<= distanceTol)
+				inliers.insert(index);
+		}
+
+		// Pick bigger set of points (line fitting more points)
+		if(inliers.size() > inliersResult.size())
+		{
+			inliersResult = inliers;
+		}
+
+	}
+
+	//TODO: add timer
 	
+	// Return indicies of inliers from fitted line with most inliers
 	return inliersResult;
 
 }
@@ -92,7 +152,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 10, 1.0);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
